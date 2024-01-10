d8438dad333f3dc938bb7a9fa567368381ec1069
Jinwoo Je <jeff.jw.je@gmail.com>
2024-01-10 13:26:18 -0600

    minor updates

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -5,10 +5,13 @@ const Welcome = () => (
   <Jumbotron>
     <h1>Images Gallery</h1>
     <p>
-This is simple application that retrieves photos using Unsplash API. In order to start enter any search term in the input field.  
+      This is simple application that retrieves photos using Unsplash API. In
+      order to start enter any search term in the input field.
     </p>
     <p>
-      <Button variant="primary" href="https://unsplash.com" target="_blank">Learn more</Button>
+      <Button variant="primary" href="https://unsplash.com" target="_blank">
+        Learn more
+      </Button>
     </p>
   </Jumbotron>
 );
